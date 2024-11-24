dd94cb21eff0fbd90a7585c94b2a63187235ea54
JulioRafaelFlores <167726991+JulioRafaelFlores@users.noreply.github.com>
2024-11-24 01:03:16 -0400

    cambiossss

--- InterfasDelTrabajo/Form1.Designer.vb
+++ InterfasDelTrabajo/Form1.Designer.vb
@@ -30,6 +30,8 @@ Partial Class Form1
         Me.DdddToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.DdgToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GgghToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -87,11 +89,31 @@ Partial Class Form1
         Me.GgghToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.GgghToolStripMenuItem.Text = "gggh"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(119, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(70, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
@@ -111,4 +133,6 @@ Partial Class Form1
     Friend WithEvents AaddaToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents GgghToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents DdddToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
